b13aedc00dbe5be294fa4db26b9ed11f0222269c
Alessandro Sperotti <alessandro.sperotti@cloudacademy.com>
2021-02-18 09:56:07 +0100

    created xcode

---
+++ LiveFullyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveFullyApp.swift
+//  LiveFully
+//
+//  Created by Alessandro Sperotti on 18/02/21.
+//
+
+import SwiftUI
+
+@main
+struct LiveFullyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
